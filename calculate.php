4c7f2b825ea3dfa40b63a6ca8a7574b6e20bafa9
Greg Randall <gregrr@gmail.com>
2018-03-16 17:27:17 -0500

    added validation changed basal metabolic rate calculator to more modern one

--- calculate.php
+++ calculate.php
@@ -1,53 +1,64 @@
 <?php
-include ("functions.php");//get_client_ip()
+include ("functions.php");
 
 include ("metslookup.php");//mets table for lookup
 
 include ("default-values.php");//defaults for values
 
+
 //get passed variables & validate:
-if($_GET["age"] && $_GET["age"]>1 && $_GET["age"]<120){
+if(is_numeric($_GET["age"])){
   $age = (int) $_GET["age"];
 }else{
 	$age=$d_age;
 }
+if($age<0){
+	$age=$d_age;
+}
 
-if($_GET["height-f"] | $_GET["height-i"]){
+if(is_numeric($_GET["height-f"]) | is_numeric($_GET["height-i"])){
   $height = inchestocm((((float) $_GET["height-f"])*12)+((float) $_GET["height-i"])); //feet *12 plus inches for height in inches
 }else{
 	$height=$d_height;
 }
-
-if($height<121 | $height>213) { // if height is less than 4 feet or greater than 7 feet
+if($height<0){
 	$height=$d_height;
 }
 
-if($_GET["sex"]){
+
+
+
+
+if(is_string($_GET["sex"])){
   $sex = strtolower(trim((string) $_GET["sex"]));
 }else{
 	$sex=$d_sex;
 }
-
 if($sex!="m" && $sex!="f"){
-
 	$sex=$d_sex;
 }
 
 
-if($_GET["weight"]){
+if(is_numeric($_GET["weight"])){
   $weight = lbstokg((float) $_GET["weight"]); //lbs
 }else{
 	$weight=$d_weight;
 }
+if($weight<0){
+	$weight=$d_weight;
+}
 
-if($_GET["speed"]){
+if(is_numeric($_GET["speed"])){
   $speed = (float) $_GET["speed"]; //mph
 }else{
 	$speed=$d_speed;
 }
+if(($speed>$metsmax)|($speed<$metsmin)){
+  $speed=$d_speed;
+}
 
 
-if($_GET["diet"]){
+if(is_string($_GET["diet"])){
   $diet = strtolower(trim( (string) $_GET["diet"] ));
 }else{
 	$diet=$d_diet;
@@ -68,29 +79,28 @@ echo "<b>Diet:</b> ". $diet ."<br>\r\n<br>\r\n<br>\r\n";
 
 
 
-if($diet=="o"){
+if($diet=="o"){//if the person is an ominivore use 10x, if they're a vegetarian, use 5x. if something weird happened use 10x
 		$fossilratio = 10;
 }else if($diet=="v"){
 	$fossilratio = 5;
+}else{
+	$fossilratio = 10;
 }
 
 
 $gallonofgas = 31520;//calories in gallon of gas
 
-
+/* https://en.wikipedia.org/wiki/Harris%E2%80%93Benedict_equation
+M BMR = (10 * weight in kg) + (6.25 * height in cm) - (5 * age in years) + 5
+W	BMR = (10 * weight in kg) + (6.25 * height in cm) - (5 * age in years) - 161 */
 if($sex == "m"){//if sex is m use the male bmr calculation
-	$bmr = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
+	$bmr = (10 * $weight ) + (6.25 * $height) - (5 * $age) + 5;
 }else{//if sex is anything other than male use the female bmr calculation
-	$bmr = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
+	$bmr = (10 * $weight ) + (6.25 * $height) - (5 * $age) - 161;
 }
 
-//make sure that the input speed is above the min and below the max in the mets table
-if(($speed<=$metsmax)&&($speed>=$metsmin)){
-	$mets = $metslookup[number_format($speed, 1)];//look up the mets based on the speed
-}else {//if the speed is out of range use the middle speed
-	$mets = 3.3; //middle speed & mets
-	$speed = 3;
-}
+$mets = $metslookup[number_format($speed, 1)];//look up the mets based on the speed
+
 
 echo "<b>Basal Metabolic Rate (bmr):</b> ". round($bmr,2) ."<br>\r\n";
 echo "<b>METS Multiplier:</b> ". $mets ."<br>\r\n<br>\r\n";
@@ -102,9 +112,10 @@ $humanmpg = round($gallonofgas/(((($bmr/24)*$mets)/$speed)*$fossilratio),2);
 echo "<h2>Human Mpg:</h2><br>\r\n";
 echo "<h1>" . $humanmpg . "mpg</h1><br>\r\n";
 
+
+//record the input for debugging
 date_default_timezone_set("America/Chicago");
 $output = "$age,$height,$sex,$weight,$speed,$mets,$humanmpg," . get_client_ip() . "," . date("Y-m-d") . "," .date("H:i:s") . ",\r\n";
-//echo $output;
 file_put_contents("record.csv", $output, FILE_APPEND);
 
 ?>
